cb60ae827dbf220b07435dc9f57d2f249605de14
John Taylor <john.taylor@onepeloton.com>
2021-09-15 15:07:35 -0400

    Update run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -4,5 +4,5 @@ docker run \
   --name mongodump-s3 \
   --rm \
   --env-file config.env \
-  --mount type=bind,source=/Users/sean/.aws,target=/root/.aws \
-  recipedude/bullseye-mongodb-s3:latest #sleep 999999
+  --mount type=bind,source=/Users/john.taylor/.aws,target=/root/.aws \
+  recipedude/bullseye-mongodb-s3:latest sleep 999999
